6ae67d45cf0898d8827a702b9187cd327ea4ea19
trainzug <philippschneeider@gmail.com>
2020-05-26 16:16:35 +0200

    grammatik geändert

--- praktikum 4.pl
+++ praktikum 4.pl
@@ -1,4 +1,16 @@
 % a)
+% Sprache: 0 oder unendlich viele a eingeschlossen von b
+% 
+% ?- lvonN(Zs).    
+% Zs = [b, b] ;
+% Zs = [b, a, b] ;
+% Zs = [b, a, a, b] ;
+% Zs = [b, a, a, a, b] ;
+% Zs = [b, a, a, a, a, b] ;
+% Zs = [b, a, a, a, a, a, b] ;
+% Zs = [b, a, a, a, a, a, a, b] ;
+% Zs = [b, a, a, a, a, a, a, a, b] ;
+
 
 % z(Zustände) :- Zustände ist die endliche Zustandmenge
 z(z0).
@@ -9,22 +21,20 @@ sigma(a).
 sigma(b).
 
 % delta(J,W,N) :- J jetziger Zustand, W ist der Übergangswert, N ist der nächste Zustand
-delta(z0,a,z1).
 delta(z0,b,z1).
-delta(z1,a,z0).
+delta(z1,a,z1).
 delta(z1,b,z0).
-
 
 % start(A) :- A ist der Startzustand
 start(z0).
 
 % start(E) :- E ist der Endzustand
-ende(z1).
+ende(z0).
 
 
 % b)
 
-% lvonN(Ws) :- Ws ist ein Wort der Sprache aus von a)
+% lvonN(Ws) :- Ws ist ein Wort der Sprache aus a)
 lvonN(Ws) :- start(Zstart), ende(Zende), delta_stern(Zstart,Ws,Zende).
 
 % delta_stern(Zakt,Ws,Zneu) :- Erweiterung von delta
